a53814bcd2ae1b7d2ca89b43c8eb9663554c621c
arampall <achyut.9316@gmail.com>
2020-04-22 12:09:01 -0700

    testing docker push script

--- udacity-c3-deployment/docker/docker-push.sh
+++ udacity-c3-deployment/docker/docker-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Starting docker push"
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push rampalli6/udacity-frontend:local
-docker push rampalli6/udacity-restapi-feed
-docker push rampalli6/udacity-restapi-user
-docker push rampalli6/reverseproxy
+#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+#docker push rampalli6/udacity-frontend:local
+#docker push rampalli6/udacity-restapi-feed
+#docker push rampalli6/udacity-restapi-user
+#docker push rampalli6/reverseproxy
